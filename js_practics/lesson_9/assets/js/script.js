ab99095af431a9deb836cfc1a3e181c86abc96ed
khuralay.m <merzhankuralay@gmail.com>
2024-02-27 11:43:18 +0600

    калькулятор with js

--- js_practics/lesson_9/assets/js/script.js
+++ js_practics/lesson_9/assets/js/script.js
@@ -6,8 +6,8 @@ function myFunction(){
     li = ul.getElementsByTagName("li");
 
     for(i = 0; i < li.length; i++){
-        a = li[i].getElementsByTagName("a")[0];
-        if(a.innerHTML.toUpperCase().indexOf(filter) > -1){
+        a = li[i].getElementsByTagName("a")[0]; //Получается ссылка на первый дочерний элемент <a> текущего элемента списка <li>.
+        if(a.innerHTML.toUpperCase().indexOf(filter) > -1){ //Проверяется, содержит ли текст элемента <a> фильтр, введенный пользователем. Если да, то стиль элемента <li> остается видимым (display = ""), иначе скрывается (display = "none").
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";

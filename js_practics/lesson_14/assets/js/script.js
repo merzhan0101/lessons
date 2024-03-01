032d2dc3083f473597b9f3d2eae3ee7400602b00
khuralay.m <merzhankuralay@gmail.com>
2024-03-01 09:44:30 +0500

    Как сделать Popup

---
+++ js_practics/lesson_14/assets/js/script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    var popup = document.getElementById('myPopup');
+    popup.classList.toggle('show');
+    // classList это псевдомассив классов элемента (который можно, к примеру, перебрать циклом).
+    // toggle - используется для добавления или удаления класса у элемента в зависимости от его наличия
+}
